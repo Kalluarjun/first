17f5457c2ee756a89fe93cfa1ff9dacfb5d82c53
Arjun Lamichhane <075bce025.arjun@pcampus.edu.np>
2024-11-20 20:17:58 +0545

    updated

--- 4.linear regression.py
+++ 4.linear regression.py
@@ -22,7 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 scaler= StandardScaler()
 X_train= scaler.fit_transform(X_train)
-X_test= scaler.transform(X_test)
+X_test= scaler.fit_transform(X_test)
 
 #cross validation
 
